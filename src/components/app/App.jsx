89ff2f0d1a172acc68559ae2713298813c33e9c5
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-06 00:31:40 +0300

    add sidebar container

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../header/Header';
+import SidebarPanel from '../shared/sidebarPanel/SidebarPanel';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <SidebarPanel />
+    </>
+  );
 };
 
 export default App;
