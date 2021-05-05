94626849a312205051c12f8a7caa99c08354bb8f
capricornus44 <antonzavalniuk@gmail.com>
2021-05-05 18:27:17 +0200

    Added responsive background for authorization page

--- src/components/auth/authWrapper/AuthWrapper.jsx
+++ src/components/auth/authWrapper/AuthWrapper.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AuthForm from '../authForm/AuthForm';
+import AuthBg from '../authBg/AuthBg';
 import './AuthWrapper.scss';
 
 const AuthWrapper = () => {
   return (
     <div className="auth-wrapper">
+      <AuthBg />
       <AuthForm />
     </div>
   );
