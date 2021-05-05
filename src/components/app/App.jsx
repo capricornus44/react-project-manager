99a5aacae172a067c5fef3ab4b7bd7a39524f83a
capricornus44 <antonzavalniuk@gmail.com>
2021-05-05 18:20:34 +0200

    Added component NavigationRoutes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Header from '../header/Header';
+import Main from '../main/Main';
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
