8e229fba7ab86f65f89d15f7dad0eeb21c820660
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-05 16:40:20 +0300

    add addButton and graphButton

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+import AddButton from '../../components/shared/addButton/AddButton';
+import GraphButton from '../../components/shared/graphButton/GraphButton';
 const App = () => {
-  return <h1>APP PAGE</h1>;
+  return (
+    <>
+      <h1>APP PAGE</h1>
+      <AddButton />
+      <GraphButton />
+    </>
+  );
 };
 
 export default App;
--- src/components/shared/graphButton/GraphButton.jsx
+++ src/components/shared/graphButton/GraphButton.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import './AddButton.scss';
+import '../addButton/AddButton.scss';
 import sprite from '../../../assets/icons/sprite.svg';
 
 const AddButton = ({ onClick }) => {
   return (
     <button
       type="button"
-      aria-label="add button"
+      aria-label="graph button"
       className="add-button"
       onClick={onClick}
     >
       <svg className="icon-plus">
-        <use href={sprite + '#plus'}></use>
+        <use href={sprite + '#analytics'}></use>
       </svg>
     </button>
   );
