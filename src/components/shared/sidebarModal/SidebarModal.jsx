345993f63be8d6a7fbafe3f296721c58c5013e85
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-06 01:14:16 +0300

    start modals

---
+++ src/components/shared/sidebarModal/SidebarModal.jsx
@@ -0,0 +1,32 @@
+// import React, { useRef } from 'react';
+// import { createPortal } from 'react-dom';
+// import './SidebarModal.scss';
+// import sprite from '../../../assets/icons/sprite.svg';
+
+// const modalRoot = document.querySelector('#modal_root');
+
+// const SidebarModal = ({ children }) => {
+//   const ShowModal = true;
+
+//   return createPortal(
+//     showModal && (
+//       <div className="sidebar-modal" ref={modalRef} onClick={closeModal}>
+//         <div className="sidebar-modal__container">
+//           <button
+//             type="button"
+//             className="sidebar-modal__btn"
+//             onClick={() => onClose()}
+//           >
+//             <svg className="sidebar-modal__icon">
+//               <use href={sprite + '#close'} />
+//             </svg>
+//           </button>
+//           {children}
+//         </div>
+//       </div>
+//     ),
+//     modalRoot,
+//   );
+// };
+
+// export default SidebarModal;
