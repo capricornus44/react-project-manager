c185098f7140b1b97f96ac460701e4e670db989b
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-05 19:06:52 +0300

    clean buttons

--- src/components/shared/deleteButton/DeleteButton.jsx
+++ src/components/shared/deleteButton/DeleteButton.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import './DeleteButton.scss';
 import sprite from '../../../assets/icons/sprite.svg';
 
-const DeleteButton = ({ onClick }) => {
+const DeleteButton = () => {
   return (
-    <button
-      type="button"
-      aria-label="delete button"
-      className="delete-button"
-      onClick={onClick}
-    >
+    <button type="button" aria-label="delete button" className="delete-button">
       <svg className="icon-delete">
         <use href={sprite + '#delete'}></use>
       </svg>
