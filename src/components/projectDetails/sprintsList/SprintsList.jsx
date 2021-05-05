d4c0197fc96d7dc945e6fd303a115c6db06c6beb
Vlad <vladhopilo@gmail.com>
2021-05-05 20:03:02 +0300

    up

--- src/components/projectDetails/sprintsList/SprintsList.jsx
+++ src/components/projectDetails/sprintsList/SprintsList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getSprints } from '../../../redux/sprints/sprintOperations';
 import SprintsListItem from '../sprintsListItem/SprintsListItem';
+import "./SprintsList.scss"
 
 const SprintsList = () => {
     console.dir(getSprints())
--- src/components/projectDetails/sprintsListItem/SprintsListItem.jsx
+++ src/components/projectDetails/sprintsListItem/SprintsListItem.jsx
@@ -1,44 +1,85 @@
 import React from 'react';
+import "./SprintsListItem.scss"
 
 const SprintsListItem = () => {
     return (<>
         <li className="sprint-item">
-        <h3>Sprint Burndown Chart 1</h3>
-        <p>Дата початку</p>
-        <p>10 Лип</p>
-        <p>Дата закінченя</p>
-        <p>22 Лип</p>
-        <p>Тривалість</p>
-        <p>226</p>
+        <h3 className="sprint-item__title">Sprint Burndown Chart 1</h3>
+        <div className="sprint-item__details">
+          <div className="sprint-item__details-name">
+              <p>Дата початку</p>
+              <p className="sprint-item__details-elem">Дата закінченя</p>
+              <p className="sprint-item__details-elem">Тривалість</p>
+          </div>
+          <div className="sprint-item__details-value">
+              <p>10 Лип</p>
+              <p className="sprint-item__details-elem">22 Лип</p>
+              <p className="sprint-item__details-elem">226</p>
+            </div>
+          </div>
       </li>
-      <li>
-        <h3>Sprint Burndown Chart 1</h3>
-        <p>Дата початку</p>
-        <p>10 Лип</p>
-        <p>Дата закінченя</p>
-        <p>22 Лип</p>
-        <p>Тривалість</p>
-        <p>226</p>
-        </li>
+      <li className="sprint-item">
+        <h3 className="sprint-item__title">Sprint Burndown Chart 1</h3>
+        <div className="sprint-item__details">
+          <div className="sprint-item__details-name">
+              <p>Дата початку</p>
+              <p className="sprint-item__details-elem">Дата закінченя</p>
+              <p className="sprint-item__details-elem">Тривалість</p>
+          </div>
+          <div className="sprint-item__details-value">
+              <p>10 Лип</p>
+              <p className="sprint-item__details-elem">22 Лип</p>
+              <p className="sprint-item__details-elem">226</p>
+            </div>
+          </div>
+      </li>
+      <li className="sprint-item">
+        <h3 className="sprint-item__title">Sprint Burndown Chart 1</h3>
+        <div className="sprint-item__details">
+          <div className="sprint-item__details-name">
+              <p>Дата початку</p>
+              <p className="sprint-item__details-elem">Дата закінченя</p>
+              <p className="sprint-item__details-elem">Тривалість</p>
+          </div>
+          <div className="sprint-item__details-value">
+              <p>10 Лип</p>
+              <p className="sprint-item__details-elem">22 Лип</p>
+              <p className="sprint-item__details-elem">226</p>
+            </div>
+          </div>
+      </li>
+      <li className="sprint-item">
+        <h3 className="sprint-item__title">Sprint Burndown Chart 1</h3>
+        <div className="sprint-item__details">
+          <div className="sprint-item__details-name">
+              <p>Дата початку</p>
+              <p className="sprint-item__details-elem">Дата закінченя</p>
+              <p className="sprint-item__details-elem">Тривалість</p>
+          </div>
+          <div className="sprint-item__details-value">
+              <p>10 Лип</p>
+              <p className="sprint-item__details-elem">22 Лип</p>
+              <p className="sprint-item__details-elem">226</p>
+            </div>
+          </div>
+      </li>
+      <li className="sprint-item">
+        <h3 className="sprint-item__title">Sprint Burndown Chart 1</h3>
+        <div className="sprint-item__details">
+          <div className="sprint-item__details-name">
+              <p>Дата початку</p>
+              <p className="sprint-item__details-elem">Дата закінченя</p>
+              <p className="sprint-item__details-elem">Тривалість</p>
+          </div>
+          <div className="sprint-item__details-value">
+              <p>10 Лип</p>
+              <p className="sprint-item__details-elem">22 Лип</p>
+              <p className="sprint-item__details-elem">226</p>
+            </div>
+          </div>
+      </li>
+      
 
-        <li>
-        <h3>Sprint Burndown Chart 1</h3>
-        <p>Дата початку</p>
-        <p>10 Лип</p>
-        <p>Дата закінченя</p>
-        <p>22 Лип</p>
-        <p>Тривалість</p>
-        <p>226</p>
-        </li>
-        <li>
-        <h3>Sprint Burndown Chart 1</h3>
-        <p>Дата початку</p>
-        <p>10 Лип</p>
-        <p>Дата закінченя</p>
-        <p>22 Лип</p>
-        <p>Тривалість</p>
-        <p>226</p>
-        </li>
 </>
     );
 };
