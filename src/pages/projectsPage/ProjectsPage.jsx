e7c5922a4c9d184ef64b86f07f7d1987f08f36c5
capricornus44 <antonzavalniuk@gmail.com>
2021-05-06 11:13:03 +0200

    Added temporary redux for auth

---
+++ src/pages/projectsPage/ProjectsPage.jsx
@@ -0,0 +1,5 @@
+const ProjectsPage = () => {
+  return <h1>PROJECTS PAGE</h1>;
+};
+
+export default ProjectsPage;
--- src/redux/auth/authReducers.js
+++ src/redux/auth/authReducers.js
@@ -1,3 +1,59 @@
+// import { combineReducers } from 'redux';
+// import { createReducer } from '@reduxjs/toolkit';
+// export {
+//   signupSuccess,
+//   signupError,
+//   signinSuccess,
+//   signinError,
+//   logoutSuccess,
+//   logoutError,
+//   getCurrentUserSuccess,
+//   getCurrentUserError,
+// } from './authActions';
+
+// const initialUserState = {
+//   email: '',
+//   id: '',
+//   accessToken: '',
+//   refreshToken: '',
+// };
+
+// const user = createReducer(initialUserState, {
+//   [signupSuccess]: (_, { payload }) => payload.data,
+//   [signinSuccess]: (_, { payload }) => payload.data,
+//   [logoutSuccess]: () => initialUserState,
+//   [getCurrentUserSuccess]: (_, { payload }) => payload,
+// });
+
+// const accessToken = createReducer(null, {
+//   [signupSuccess]: (_, { payload }) => payload.accessToken,
+//   [signinSuccess]: (_, { payload }) => payload.accessToken,
+//   [logoutSuccess]: () => null,
+// });
+
+// const refreshToken = createReducer(null, {
+//   [signupSuccess]: (_, { payload }) => payload.refreshToken,
+//   [signinSuccess]: (_, { payload }) => payload.refreshToken,
+//   [logoutSuccess]: () => null,
+// });
+
+// // const setError = (_, { payload }) => payload;
+
+// const error = createReducer(null, {
+//   [signupError]: setError,
+//   [signinError]: setError,
+//   [logoutError]: setError,
+//   [getCurrentUserError]: setError,
+// });
+
+// export default combineReducers({
+//   user,
+//   accessToken,
+//   refreshToken,
+//   error,
+// });
+
+// !! template version
 import { createReducer } from '@reduxjs/toolkit';
 import { signup, signin, logout } from './authActions';
 
