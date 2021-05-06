e28381cee293661008e9dde0e1b2e87e95020030
Vlad <vladhopilo@gmail.com>
2021-05-06 18:42:09 +0300

    up

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import Container from '../container/Container';
+
 import Header from '../header/Header';
-import Main from '../main/Main';
+import ProjectDetails from '../projectDetails/ProjectDetails';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Main />
+      <Container>
+        <ProjectDetails />
+      </Container>
     </>
   );
 };
