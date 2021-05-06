023f65c99f7ef8f925d49c16142ea635292a98f8
Vlad <vladhopilo@gmail.com>
2021-05-06 14:25:22 +0300

    up

--- src/components/projectDetails/titleProjectForm/TitleProjectForm.jsx
+++ src/components/projectDetails/titleProjectForm/TitleProjectForm.jsx
@@ -9,12 +9,13 @@ const TitleProjectForm = () => {
     <>
         <div>
             <h2 className="project__details-title">
-                    {/* <img src={edit} className="project__details-edit" alt="edit" width="20px" height="20px" /> */}
-                <button className="project__details-edit__button" type="button" aria-label="edit button">
+                   
+                <button className="project__details-edit__button project__details-edit " type="button" aria-label="edit button">
                     <svg className="project__details-edit__icon"> 
-                        <use href={sprite + '#edit'}/>
+                        <use href={sprite + '#edit'}/> 
                     </svg>
                 </button>
+                    
                     Project 1
              </h2>
 
