450b67c4c9b6fd856f0afa2dfd7e9ac7cc7b4c83
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-06 13:56:16 +0300

    add modal

--- src/components/shared/sidebarModal/SidebarModal.jsx
+++ src/components/shared/sidebarModal/SidebarModal.jsx
@@ -1,32 +1,75 @@
 import React, { useRef } from 'react';
-// import { createPortal } from 'react-dom';
-// import './SidebarModal.scss';
-// import sprite from '../../../assets/icons/sprite.svg';
+import { createPortal } from 'react-dom';
+import './SidebarModal.scss';
+import sprite from '../../../assets/icons/sprite.svg';
+import FormButton from '../formButton/FormButton';
 
-// const modalRoot = document.querySelector('#modal_root');
+const modalRoot = document.querySelector('#modal_root');
 
-// const SidebarModal = ({ children }) => {
-//   const ShowModal = true;
-// const closeModal=(e)=>{}
-//   return createPortal(
-//     showModal && (
-//       <div className="sidebar-modal" ref={modalRef} onClick={closeModal}>
-//         <div className="sidebar-modal__container">
-//           <button
-//             type="button"
-//             className="sidebar-modal__btn"
-//             onClick={() => onClose()}
-//           >
-//             <svg className="sidebar-modal__icon">
-//               <use href={sprite + '#close'} />
-//             </svg>
-//           </button>
-//           {children}
-//         </div>
-//       </div>
-//     ),
-//     modalRoot,
-//   );
-// };
+const SidebarModal = ({ children, onClose, setShowModal, title }) => {
+  const modalRef = useRef();
 
-// export default SidebarModal;
+  useEffect(() => {
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  });
+  useEffect(() => {
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  });
+
+  const handleEsc = e => {
+    if (e.code === 'Escape') {
+      setShowModal(false);
+    }
+  };
+  const onHandleClick = () => {
+    setShowModal(false);
+  };
+
+  const handleBackdropClick = e => {
+    if (e.target.dataset) {
+      if (e.target.nodeName === 'DIV' && e.target.dataset.zone === 'overlay') {
+        setShowModal(false);
+      }
+    }
+  };
+
+  //   const closeModal = e => {
+  //     if (e.target === modalRef.current) {
+  //       setShowModal(false);
+  //     }
+  //   };
+  return createPortal(
+    showModal && (
+      <div
+        data-zone="overlay"
+        className="sidebar-modal"
+        ref={modalRef}
+        onClick={handleBackdropClick}
+      >
+        <div className="sidebar-modal__container">
+          <button
+            type="button"
+            className="sidebar-modal__btn"
+            onClick={onHandleClick}
+          >
+            <svg className="sidebar-modal__icon">
+              <use href={sprite + '#close'} />
+            </svg>
+          </button>
+          <h2>{title}</h2>
+          {children}
+          <FormButton onClose={onClose} />
+        </div>
+      </div>
+    ),
+    modalRoot,
+  );
+};
+
+export default SidebarModal;
