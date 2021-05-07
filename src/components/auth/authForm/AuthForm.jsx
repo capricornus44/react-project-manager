ac763b130f6873465a0004b7bdf5658a8eb6e952
capricornus44 <antonzavalniuk@gmail.com>
2021-05-07 18:41:04 +0200

    rewritten authorization form with Formik and Yup

--- src/components/auth/authForm/AuthForm.jsx
+++ src/components/auth/authForm/AuthForm.jsx
@@ -8,35 +8,38 @@ import {
 import FormButton from '../../shared/formButton/FormButton';
 import './AuthForm.scss';
 
+import { validationSchema } from '../validation/signupSchema';
+import { Formik, Form, Field } from 'formik';
+
 const AuthForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
   const location = useLocation();
   const dispatch = useDispatch();
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    if (name === 'email') setEmail(value);
-    if (name === 'password') setPassword(value);
-    if (name === 'confirmPassword') setConfirmPassword(value);
-  };
+  // const handleChange = e => {
+  //   const { name, value } = e.target;
+  //   if (name === 'email') setEmail(value);
+  //   if (name === 'password') setPassword(value);
+  //   if (name === 'confirmPassword') setConfirmPassword(value);
+  // };
 
-  const resetInputs = () => {
-    setEmail('');
-    setPassword('');
-    setConfirmPassword('');
-  };
+  // const resetInputs = () => {
+  //   setEmail('');
+  //   setPassword('');
+  //   setConfirmPassword('');
+  // };
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  // const handleSubmit = e => {
+  //   e.preventDefault();
 
-    isSignupForm()
-      ? dispatch(signupOperation({ email, password })) // вставить нужное название операции регистрации
-      : dispatch(signinOperation({ email, password })); // вставить нужное название операции логинизации
+  //   isSignupForm()
+  //     ? dispatch(signupOperation({ email, password })) // вставить нужное название операции регистрации
+  //     : dispatch(signinOperation({ email, password })); // вставить нужное название операции логинизации
 
-    resetInputs();
-  };
+  //   resetInputs();
+  // };
 
   const isSignupForm = () => {
     return location.pathname === '/signup';
@@ -44,83 +47,191 @@ const AuthForm = () => {
 
   return (
     <>
-      <form className="auth-form" onSubmit={handleSubmit}>
-        <h1 className="auth-form__title">
-          {isSignupForm() ? 'Реєстрація' : 'Вхід'}
-        </h1>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+          confirmPassword: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={({ email, password, confirmPassword }) => {
+          // if (password !== confirmPassword) {
+          //   error.setErrors('passwords do not match');
+          //   return;
+          // }
 
-        <div className="auth-form__group">
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder=" "
-            // autoComplete="off"
-            required
-            onChange={handleChange}
-            id="email"
-            className="auth-form__input"
-          />
-          <label className="auth-form__label" htmlFor="email">
-            Email
-          </label>
-        </div>
+          if (isSignupForm()) {
+            dispatch(signupOperation({ email, password }));
+          } else {
+            dispatch(signinOperation({ email, password }));
+          }
+        }}
+      >
+        {({ values, errors, touched }) => (
+          <Form className="auth-form">
+            <h1 className="auth-form__title">
+              {isSignupForm() ? 'Реєстрація' : 'Вхід'}
+            </h1>
 
-        <div className="auth-form__group">
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder=" "
-            // autoComplete="off"
-            required
-            onChange={handleChange}
-            id="password"
-            className="auth-form__input"
-          />
-          <label className="auth-form__label" htmlFor="password">
-            Пароль
-          </label>
-        </div>
+            <div className="auth-form__group">
+              <Field
+                type="email"
+                name="email"
+                value={values.email}
+                placeholder=" "
+                // autoComplete="off"
+                id="email"
+                className="auth-form__input"
+              />
+              <label className="auth-form__label" htmlFor="email">
+                Email
+              </label>
 
-        {isSignupForm() && (
-          <div className="auth-form__group">
-            <input
-              type="password"
-              name="confirmPassword"
-              value={confirmPassword}
-              placeholder=" "
-              // autoComplete="off"
-              required
-              onChange={handleChange}
-              id="confirmPassword"
-              className="auth-form__input"
-            />
-            <label className="auth-form__label" htmlFor="confirmPassword">
-              Повторіть пароль
-            </label>
-          </div>
+              {errors.email && touched.email ? (
+                <p className="error">{errors.email}</p>
+              ) : null}
+            </div>
+
+            <div className="auth-form__group">
+              <Field
+                type="password"
+                name="password"
+                value={values.password}
+                placeholder=" "
+                // autoComplete="off"
+                id="password"
+                className="auth-form__input"
+              />
+              <label className="auth-form__label" htmlFor="password">
+                Пароль
+              </label>
+              {errors.password && touched.password ? (
+                <p className="error">{errors.password}</p>
+              ) : null}
+            </div>
+
+            {isSignupForm() && (
+              <div className="auth-form__group">
+                <Field
+                  type="password"
+                  name="confirmPassword"
+                  value={values.confirmPassword}
+                  placeholder=" "
+                  // autoComplete="off"
+                  id="confirmPassword"
+                  className="auth-form__input"
+                />
+                <label className="auth-form__label" htmlFor="confirmPassword">
+                  Повторіть пароль
+                </label>
+                {errors.confirmPassword && touched.confirmPassword ? (
+                  <p className="error">{errors.confirmPassword}</p>
+                ) : null}
+              </div>
+            )}
+
+            <FormButton>
+              {isSignupForm() ? 'Зареєструватися' : 'Увійти'}
+            </FormButton>
+
+            {isSignupForm() ? (
+              <p className="auth-form__redirect-question">
+                Маєте акаунт?
+                <NavLink to="/signin" className="auth-form__redirect-link">
+                  Увійти
+                </NavLink>
+              </p>
+            ) : (
+              <p className="auth-form__redirect-question">
+                Немає акаунту?
+                <NavLink to="/signup" className="auth-form__redirect-link">
+                  Зареєструватись
+                </NavLink>
+              </p>
+            )}
+          </Form>
         )}
-
-        <FormButton>{isSignupForm() ? 'Зареєструватися' : 'Увійти'}</FormButton>
-
-        {isSignupForm() ? (
-          <p className="auth-form__redirect-question">
-            Маєте акаунт?
-            <NavLink to="/signin" className="auth-form__redirect-link">
-              Увійти
-            </NavLink>
-          </p>
-        ) : (
-          <p className="auth-form__redirect-question">
-            Немає акаунту?
-            <NavLink to="/signup" className="auth-form__redirect-link">
-              Зареєструватись
-            </NavLink>
-          </p>
-        )}
-      </form>
+      </Formik>
     </>
+
+    // <>
+    //   <form className="auth-form" onSubmit={handleSubmit}>
+    //     <h1 className="auth-form__title">
+    //       {isSignupForm() ? 'Реєстрація' : 'Вхід'}
+    //     </h1>
+
+    //     <div className="auth-form__group">
+    //       <input
+    //         type="email"
+    //         name="email"
+    //         value={email}
+    //         placeholder=" "
+    //         // autoComplete="off"
+    //         required
+    //         onChange={handleChange}
+    //         id="email"
+    //         className="auth-form__input"
+    //       />
+    //       <label className="auth-form__label" htmlFor="email">
+    //         Email
+    //       </label>
+    //     </div>
+
+    //     <div className="auth-form__group">
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         value={password}
+    //         placeholder=" "
+    //         // autoComplete="off"
+    //         required
+    //         onChange={handleChange}
+    //         id="password"
+    //         className="auth-form__input"
+    //       />
+    //       <label className="auth-form__label" htmlFor="password">
+    //         Пароль
+    //       </label>
+    //     </div>
+
+    //     {isSignupForm() && (
+    //       <div className="auth-form__group">
+    //         <input
+    //           type="password"
+    //           name="confirmPassword"
+    //           value={confirmPassword}
+    //           placeholder=" "
+    //           // autoComplete="off"
+    //           required
+    //           onChange={handleChange}
+    //           id="confirmPassword"
+    //           className="auth-form__input"
+    //         />
+    //         <label className="auth-form__label" htmlFor="confirmPassword">
+    //           Повторіть пароль
+    //         </label>
+    //       </div>
+    //     )}
+
+    //     <FormButton>{isSignupForm() ? 'Зареєструватися' : 'Увійти'}</FormButton>
+
+    //     {isSignupForm() ? (
+    //       <p className="auth-form__redirect-question">
+    //         Маєте акаунт?
+    //         <NavLink to="/signin" className="auth-form__redirect-link">
+    //           Увійти
+    //         </NavLink>
+    //       </p>
+    //     ) : (
+    //       <p className="auth-form__redirect-question">
+    //         Немає акаунту?
+    //         <NavLink to="/signup" className="auth-form__redirect-link">
+    //           Зареєструватись
+    //         </NavLink>
+    //       </p>
+    //     )}
+    //   </form>
+    // </>
   );
 };
 
