e1493f3e6474d1191cd7fed79e7ceb6a52864edf
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-08 14:41:52 +0300

    try

--- src/components/graph/Graph.jsx
+++ src/components/graph/Graph.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 
 const Graph = () => {
+  const getAll = 250; //здесь должна быть сумма всех тасков
+  //(в этих тасках должны быть плановые и фактические)
+  //вытянуть из данных Влада useSelector или другим способом
+
+  // const plannedLine=()=>{достать из getAll планируемые }
+  //const otherDayPlannedLine=()=>{планируемая линия -=планируемые/getAll}
+  //const otherDayFactLine=()=>{фактическая линия-=остаток предыдущего дня}
   const months = [
     '',
     'JAN',
@@ -17,6 +24,7 @@ const Graph = () => {
     'NOV',
     'DEC',
   ];
+
   const data = {
     // labels: result,
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -40,8 +48,9 @@ const Graph = () => {
         pointHoverBorderWidth: 2,
         pointRadius: 3,
         pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [65, 59, 80, 81, 56, 55, 40], //[plannedLine,...otherDayFactLine]
       },
+
       {
         label: 'Planned remaining work in hours',
         fill: false,
@@ -61,7 +70,7 @@ const Graph = () => {
         pointHoverBorderWidth: 2,
         pointRadius: 3,
         pointHitRadius: 10,
-        data: [250, 242, 236, 229, 221, 214, 208],
+        data: [250, 242, 236, 229, 221, 214, 208], //[plannedLine,...otherDayPlannedLine]
       },
     ],
   };
