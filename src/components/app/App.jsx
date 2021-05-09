13921d174c8912441024393e789197f76443063e
capricornus44 <antonzavalniuk@gmail.com>
2021-05-09 16:02:41 +0200

    Added authorization form translation

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Header from '../header/Header';
 import Main from '../main/Main';
+import { useLanguage } from '../../hooks/useLanguage';
+
+export const LangContext = createContext();
 
 const App = () => {
+  const [language, setLanguage, list] = useLanguage();
   return (
-    <>
+    <LangContext.Provider value={{ language, setLanguage, list }}>
       <Header />
       <Main />
-    </>
+    </LangContext.Provider>
   );
 };
 export default App;
