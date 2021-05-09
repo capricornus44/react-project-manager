62d7cc24feeadb1e8f01d986bff13cf7d969282c
capricornus44 <antonzavalniuk@gmail.com>
2021-05-09 15:59:34 +0200

    Added English and Ukrainian languages

---
+++ src/languages/english.js
@@ -0,0 +1,22 @@
+const english = {
+  title: 'english',
+  name: 'ENG',
+  userMenu: {
+    exit: 'Log out',
+  },
+  authForm: {
+    signupTitle: 'Registration',
+    signinTitle: 'Authentication',
+    email: 'Email',
+    password: 'Password',
+    confirmPassword: 'Confirm password',
+    signupButton: 'Sign up',
+    signinButton: 'Sign in',
+    signupQuestion: 'Have you got an account?',
+    signinQuestion: "Haven't got an account yet?",
+    signupLink: 'Sign in',
+    signinLink: 'Sign up',
+  },
+};
+
+export { english };
---
+++ src/languages/ukrainian.js
@@ -0,0 +1,22 @@
+const ukrainian = {
+  title: 'ukrainian',
+  name: 'UKR',
+  userMenu: {
+    exit: 'Вихiд',
+  },
+  authForm: {
+    signupTitle: 'Реєстрація',
+    signinTitle: 'Вхід',
+    email: 'Email',
+    password: 'Пароль',
+    confirmPassword: 'Підтвердіть пароль',
+    signupButton: 'Зареєструватися',
+    signinButton: 'Увійти',
+    signupQuestion: 'Маєте акаунт?',
+    signinQuestion: 'Немає акаунту?',
+    signupLink: 'Увійти',
+    signinLink: 'Зареєструватись',
+  },
+};
+
+export { ukrainian };
