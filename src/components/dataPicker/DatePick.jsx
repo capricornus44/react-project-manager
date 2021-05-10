d070e93405c90a57e5bb44d9b9e545d02c88ead2
Olena Abramova <olena.v.abramova@gmail.com>
2021-05-10 12:29:58 +0300

    change language datepicker

--- src/components/dataPicker/DatePick.jsx
+++ src/components/dataPicker/DatePick.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { useState } from 'react';
-import DatePicker from 'react-datepicker';
-// import { registerLocale } from 'react-datepicker';
-// import ua from 'date-fns/locale/ua';
-// import 'dayjs/locale/ua';
+import DatePicker, { registerLocale, setDefaultLocale } from 'react-datepicker';
 import './DatePick.scss';
+import uk from 'date-fns/locale/uk';
+const locale = 'uk';
+registerLocale('uk', uk);
+setDefaultLocale('uk');
+// const spell = {
+//   spellcheck: 'false',
+// };
 
 const DatePick = () => {
   const [startDate, setStartDate] = useState(new Date());
   //   registerLocale('ua', ua);
   return (
-    <div className="picker_container">
+    <div className="picker_container" spellcheck="false">
       <label htmlFor="picker_label">
         <span className="picker_span">Дата початку</span>
       </label>
       <DatePicker
-        // locale="ua"
+        locale={locale}
         className="picker"
         selected={startDate}
         onChange={date => setStartDate(date)}
         id="picker"
-        dateFormat="MMMM dd"
+        dateFormat="dd MMMM"
       />
     </div>
   );
