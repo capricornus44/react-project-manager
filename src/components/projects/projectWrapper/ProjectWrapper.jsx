518dfe519ae6708fb993939bef81ab74ed24eac4
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-05-13 21:59:10 +0300

    add transition css

--- src/components/projects/projectWrapper/ProjectWrapper.jsx
+++ src/components/projects/projectWrapper/ProjectWrapper.jsx
@@ -5,20 +5,11 @@ import ProjectsHeader from '../ptojectsHeader/ProjectsHeader';
 import './ProjWrapper.scss';
 
 const ProjectWrapper = () => {
-  // const projects = useSelector(state => state.projects);
-
   return (
     <div className="proj_wrapper">
       <ProjectsHeader />
+
       <ProjectsList />
-      {/* {projects.length > 0 ? (
-        <ProjectsList />
-      ) : (
-        <h2 className="proj_default">
-          Ваша колекція проектів порожня, скористайтесь кнопкою "Створити
-          проект"
-        </h2>
-      )} */}
     </div>
   );
 };
